613626501a5ade40ccec97f3efa992bca128b8b9
Johan van der Merwe <johannesmerwe@gmail.com>
2023-06-16 14:53:19 +0100

    Initial commit
    Initial commit

---
+++ src/components/Accordion/index.tsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
+
+export interface AccordionItemProps {
+  id: string;
+  label: string;
+  content: string;
+}
+
+export interface AccordionProps {
+  items: AccordionItemProps[];
+}
+
+const AccordionContainer = styled.div`
+  font-family: Arial, sans-serif;
+  border-radius: 15px;
+  overflow: hidden;
+`;
+
+const AccordionButton = styled.button<{ active: boolean }>`
+  color: #fff;
+  background-color: #a61208;
+  border: none;
+  padding: 12px;
+  font-size: 16px;
+  text-align: left;
+  width: 100%;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #a61208;
+  }
+
+  ${(props) =>
+    props.active &&
+    css`
+      background-color: #a61208;
+    `}
+`;
+
+const AccordionIcon = styled.span<{ active: boolean }>`
+  border: solid white;
+  border-width: 0 2px 2px 0;
+  display: block;
+  height: 8px;
+  pointer-events: none;
+  width: 8px;
+  margin: 0 15px 0 auto;
+  float: right;
+
+  ${(props) =>
+    (props.active &&
+      css`
+        transform: translateY(2px) rotate(-135deg);
+      `) ||
+    css`
+      transform: translateY(-2px) rotate(45deg);
+    `}
+`;
+
+const AccordionSection = styled.section<{ active: boolean }>`
+  background-color: #fafafa;
+  padding: 12px;
+  transition: max-height 0.3s;
+
+  ${(props) =>
+    !props.active &&
+    css`
+      max-height: 0;
+      overflow: hidden;
+      padding: 0;
+      border: none;
+    `}
+`;
+
+export default function Accordion({ items }: AccordionProps) {
+  const [activeID, setActiveID] = useState<string | null>(null);
+
+  const toggleAccordion = (id: string) => {
+    setActiveID((prevID) => (prevID === id ? null : id));
+  };
+
+  return (
+    <AccordionContainer>
+      {items.map(({ id, label, content }) => (
+        <div key={id}>
+          <AccordionButton
+            type="button"
+            active={activeID === id}
+            onClick={() => toggleAccordion(id)}
+            aria-expanded={activeID === id ? "true" : "false"}
+          >
+            {label}
+            <AccordionIcon active={activeID === id} aria-hidden="true" />
+          </AccordionButton>
+          {activeID === id && (
+            <AccordionSection active={activeID === id}>
+              {content}
+            </AccordionSection>
+          )}
+        </div>
+      ))}
+    </AccordionContainer>
+  );
+}
